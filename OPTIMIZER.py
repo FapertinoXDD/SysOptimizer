7d5cbe502c8c38c0ec5fee370fd486997d1bfd5f
Fapertino <76165188+FapertinoXDD@users.noreply.github.com>
2022-05-06 21:09:28 +0300

    Add files via upload

---
+++ OPTIMIZER.py
@@ -0,0 +1,57 @@
+import time
+import sys
+
+time.sleep(1)
+print('Оптимизация системы')
+print('Версия 1.1')
+time.sleep(1)
+print('+ для оптимизаиции')
+print('- Для отмены')
+O = input('')
+
+if O == "+":
+	print('Оптимизация системы:')
+	print('%1')
+	time.sleep(2)
+	print('%3')
+	time.sleep(3)
+	print('%5')
+	time.sleep(5)
+	print('%7')
+	print('%10')
+	time.sleep(4)
+	print('%14')
+	print('%23')
+	print('%25')
+	print('%30')
+	time.sleep(1)
+	print('%37')
+	print('%40')
+	print('%69')
+	time.sleep(4)
+	print('%73')
+	print('%80')
+	print('%85')
+	print('%93')
+	time.sleep(3)
+	print('%99')
+	time.sleep(2)
+	print('%99.7')
+	time.sleep(8)
+	print('Оптимизация отменена')
+	print('Причина: Сервер не отвечает.....')
+	time.sleep(1)
+	print('Повторите попытку позже') 
+	time.sleep(4)
+	print('                   By FunnyProject')
+	time.sleep(5)
+
+elif O == "-":
+	print('Оптимизация отменена пользователем') 
+	print('                   By FunnyProject')
+time.sleep(3)
+print('Прочитай ПС))')
+
+sys.exit()
+
+#By Fapertino 
